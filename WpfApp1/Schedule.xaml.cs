e89315e8d4147cb6f425e0b183b82390525b8688
Cameron Perkins <camperk320@outlook.com>
2020-09-10 14:23:18 -0400

    Added Schedule/Minor fixes

--- WpfApp1/Schedule.xaml.cs
+++ WpfApp1/Schedule.xaml.cs
@@ -13,40 +13,23 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
-using Microsoft.Win32;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class Schedule : MetroWindow
     {
-        public MainWindow()
+        public Schedule()
         {
             InitializeComponent();
-            // Functions to fill each tab's DataGrid
-            
-            fill_SO1(); 
 
         }
 
         private void btn_Generate_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.InitialDirectory = "";
-            ofd.Title = "Open CSV File";
-            ofd.CheckFileExists = true;
-            ofd.CheckPathExists = true;
-            ofd.RestoreDirectory = true;
-            ofd.Filter = "CSV Files (L*.csv)|L*.csv";
-            ofd.FilterIndex = 2;
 
-            if (ofd.ShowDialog() == true)
-            {
-                Mouse.OverrideCursor = Cursors.Wait;
-                string filename = ofd.FileName;
-            }
         }
 
         private void btn_Export_Click(object sender, RoutedEventArgs e)
@@ -55,12 +38,6 @@ namespace WpfApp1
         }
 
         private void btn_Settings_Click(object sender, RoutedEventArgs e)
-        {
-            Settings form = new Settings();
-            form.Show();
-        }
-
-        private void fill_SO1()
         {
 
         }
